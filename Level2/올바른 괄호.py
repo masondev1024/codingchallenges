6e19036a7e589123ec29928dc8957259e622ddfc
masondev1024 <masondev1024@gmail.com>
2026-03-29 22:50:27 +0900

    올바른 괄호 복습

--- Level2/올바른 괄호.py
+++ Level2/올바른 괄호.py
@@ -2,7 +2,7 @@
 # 레벨: lv2
 # 날짜: 2026-03-28
 
-# 내 풀이
+# 내 풀이 <- 엄밀히 말하면 이건 스택으로 푼게 반쯤 맞음. 왜냐하면 스택에 다 쌓으면서 푼게 아니라 '('만 스택에 쌓고 ')'는 스택에 넣지 않고 trash로 취급했기 때문
 def solution(s):
     answer = True
     stack =[]  
@@ -35,4 +35,29 @@ def is_pair(s):
                 return False
 
     return len(st) == 0
+
+# 내가 다시 풀었을 떄 풀이 2
+def solution(s):
+    stack= []
+    answer=True
+    for i in s:
+        if len(stack)==0:
+            if i != '(':
+                answer=False
+                break
+            else:
+                stack.append(i)
+                continue
+        if i ==')':
+            stack.pop()
+        else:
+            stack.append(i)
+    if len(stack) !=0 :
+        answer = False
+    
+    
+    return answer
+            
+
+
 
