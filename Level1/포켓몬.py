f99367f1ee4658713df6275100571304d7dfbae9
mason <masondev1024@gmail.com>
2026-03-31 01:15:05 +0900

    풀이업데이트

---
+++ Level1/포켓몬.py
@@ -0,0 +1,29 @@
+# 프로그래머스 - 포켓몬
+# 레벨: 1
+# 날짜: 2026-03-31
+
+from collections import Counter
+def solution(nums):
+    answer = 0
+    total = len(nums)
+    pure = Counter(nums)
+    sub = len(pure) 
+    if total/2 > sub : 
+        answer = sub
+    else:
+        answer = total/2
+    return answer
+
+# 다른 사람 풀이 1
+def solution(ls):
+    return min(len(ls)/2, len(set(ls)))
+
+
+# 풀이 2
+
+def solution(nums):
+    num = {}
+    for i in nums:
+        if i not in num:
+            num[i] = True
+    return min(len(nums)/2, len(num))
